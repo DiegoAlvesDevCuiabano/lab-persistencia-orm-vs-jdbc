dbb94033082736e4128e722361de09288ad7d516
Diego Alves <diegohalves.dev@gmail.com>
2026-01-09 16:14:28 -0400

    chore: estruturar projeto base do laboratório

---
+++ src/main/java/br/com/diego/lab/persistencia/DataPersistenceEngineeringLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego.lab.persistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPersistenceEngineeringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPersistenceEngineeringLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diego/lab/persistencia/DataPersistenceEngineeringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego.lab.persistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPersistenceEngineeringLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
